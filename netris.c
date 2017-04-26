aebfd80c8c5a2d9b695a517cfebeeb676b636aff
ProfessorG <grant.vandomelen@gmail.com>
2017-04-25 22:14:28 -0700

    Started keyboard input

--- netris.c
+++ netris.c
@@ -3,22 +3,45 @@
 #define SDL_MAIN_HANDLED
 #include "SDL2/SDL.h"
 
-#define WINDOW_WIDTH 400
-#define WINDOW_HEIGHT 300
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 512
+#define NUM_OFFSETS 16
+#define STACK_SIZE 1024
 
-void update();
-void draw(SDL_Renderer*);
+#define RESOURCE_PATH "resources/"
+
+// Functions
+SDL_bool update();
+void draw();
+SDL_bool menu_update(SDL_Keycode);
+
+// Static variables
+
+SDL_Window* window;
+SDL_Surface* surface;
+
+#define FLAG_TRUE       0b11111111
+Uint8 flags;
+#define FLAG_IN_MENU    0b10000000
+#define FLAG_PAUSED     0b01000000
+
+Uint8* offsets;
+Uint8* stack;
 
 int main(int argc, char* argv[])
 {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_CreateWindowAndRenderer
+    window = SDL_CreateWindow
         (
-         WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer
+         "netris",
+         SDL_WINDOWPOS_UNDEFINED,
+         SDL_WINDOWPOS_UNDEFINED,
+         WINDOW_WIDTH,
+         WINDOW_HEIGHT,
+         0
         );
+    surface = SDL_GetWindowSurface(window);
 
     if (window == NULL)
     {
@@ -26,29 +49,29 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    if (renderer == NULL)
+    if (surface == NULL)
     {
-        printf("Could not create renderer: %s\n", SDL_GetError());
+        printf("Could not create surface: %s\n", SDL_GetError());
         return 1;
     }
 
+    // Init memory
+    flags = 0b00000000;
+    flags |= FLAG_IN_MENU & FLAG_TRUE;
+    offsets = (Uint8*) SDL_malloc(sizeof(Uint8) * NUM_OFFSETS);
+    stack = (Uint8*) SDL_malloc(sizeof(Uint8) * STACK_SIZE);
+    SDL_memset(offsets, 0, sizeof(Uint8) * NUM_OFFSETS);
+    SDL_memset(stack, 0, sizeof(Uint8) * STACK_SIZE);
+
     // The game loop
     SDL_bool done = SDL_FALSE;
-    SDL_Event event;
     while (!done)
     {
-        update();
-        draw(renderer);
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                done = SDL_TRUE;
-            }
-        }
+        done = update();
+        draw();
     }
 
-    SDL_DestroyRenderer(renderer);
+    SDL_FreeSurface(surface);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
@@ -56,13 +79,101 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void update()
-{
+// Offsets:
+//  0   Stack pointer
+//  1
+//  2
+//  3
+//  4
+//  5
+//  6
+//  7
+//  8
+//  9
+//  A
+//  B
+//  C
+//  D
+//  E
+//  F
 
+SDL_bool update()
+{
+    // Variables
+    SDL_Event event;
+    SDL_bool done = SDL_FALSE;
+
+    // Main update code
+
+    // Event Handling
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            done = SDL_TRUE;
+        }
+        if (event.type == SDL_KEYUP)
+        {
+            if (flags & FLAG_IN_MENU)
+            {
+                done = menu_update(event.key.keysym.sym);
+            }
+            else
+            {
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    done = SDL_TRUE;
+                }
+            }
+        }
+    }
+    return done;
 }
 
-void draw(SDL_Renderer* renderer)
+void draw()
 {
 
+    // Main draw code
+    if (flags & FLAG_IN_MENU)
+    {
+
+    }
+
+    //Update
+    SDL_UpdateWindowSurface(window);
+}
+
+SDL_bool menu_update(SDL_Keycode key)
+{
+    SDL_bool done = SDL_FALSE;
+    if (key == SDLK_ESCAPE)
+    {
+        if (flags & FLAG_PAUSED)
+        {
+            flags ^= FLAG_PAUSED | FLAG_IN_MENU;
+        }
+        else
+        {
+            flags ^= FLAG_IN_MENU;
+            done = SDL_TRUE;
+        }
+    }
+    if (key == SDLK_s)
+    {
+        // Menu down
+    }
+    if (key == SDLK_w)
+    {
+        // Menu up
+    }
+    if (key == SDLK_a)
+    {
+        // Menu left
+    }
+    if (key == SDLK_d)
+    {
+        // Menu right
+    }
+    return done;
 }
 
